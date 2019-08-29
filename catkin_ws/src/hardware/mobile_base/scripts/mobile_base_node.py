e672c3b6f465afb99dbe01516491180ee515c84d
eli-baron <ehecatl.eli.ba@gmail.com>
2019-08-28 20:46:54 -0500

    mofify mobile_base_node to work with ackermann_msgs

--- catkin_ws/src/hardware/mobile_base/scripts/mobile_base_node.py
+++ catkin_ws/src/hardware/mobile_base/scripts/mobile_base_node.py
@@ -1,44 +1,39 @@
 #! /usr/bin/env python
 
 import rospy
+import tf
 from roboclaw import Roboclaw
-from geometry_msgs.msg import Twist
 from ackermann_msgs.msg import AckermannDrive
 
-global msg
-msg=AckermannDrive()
-
-#def cmd_vel_callback(twist):
-	#global linear_x
-#	linear_x=twist.linear.x
-#	rospy.loginfo(twist.linear.x)
-#	linear_x=twist.linear.x
-#	msg.linear.x=twist.linear.x
-
-#def roboclaw_test_node():
-	#rospy.Subscriber("cmd_vel", Twist, cmd_vel_callback)
-	#rospy.spin()
-def ackermann_cmd_callback(data):
-	msg.speed=data.speed
+def callbackAckermann(msg):
+	global speed
+	speed=msg.speed
 
 def main():
+	global speed
+
+	speed = 0
+
+	msgAckermann = AckermannDrive()
 	rospy.init_node('mobile_base_node', anonymous=True)
-	#rospy.Subscriber("cmd_vel", Twist, cmd_vel_callback)
-	rospy.Subscriber("ackermann_cmd_topic", AckermannDrive, ackermann_cmd_callback)
-	#global linear_x
+	rospy.Subscriber("/ackermann", AckermannDrive, callbackAckermann)
 	rate=rospy.Rate(10)
 	rc=Roboclaw("/dev/ttyACM0",115200)
 	rc.Open()
 	address=0x80
+	br = tf.TransformBroadcaster()
+	autobot_x = 0
 
 	while not rospy.is_shutdown():
-		rospy.loginfo(msg.speed)
-		rc.ForwardM1(address,int(msg.speed))		#1/4 power forward = 32
-		rc.BackwardM2(address,int(msg.speed))	#1/4 power backward = 32
+		autobot_x += speed*0.001
+		msgAckermann.speed = speed
+		rc.ForwardBackwardM1(address,int(msgAckermann.speed))		#0 power forward = 64
+		rc.ForwardBackwardM1(address,int(msgAckermann.speed))	#0 power backward = 64
 		rate.sleep()
 	#rospy.spin()
 
 if __name__=='__main__':
-	#global linear_x
-	#roboclaw_test_node()
-	main()
+	try:
+    	main()
+    except rospy.ROSInterruptException:
+        pass
