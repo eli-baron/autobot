5ca43e1a823ab424754d413a9d414e9229dcf2ce
eli-baron <ehecatl.eli.ba@gmail.com>
2019-08-29 20:56:20 -0500

    add necesary code to move the wheels

--- catkin_ws/src/autobot_teleop/scripts/autobot_teleop_joy.py
+++ catkin_ws/src/autobot_teleop/scripts/autobot_teleop_joy.py
@@ -10,9 +10,7 @@ def callbackJoy(msg):
     leftStickY = msg.axes[1]    #The left stick Y controls the speed of the motors
     rightStickX = msg.axes[3]   #The right stick X controls the steering angle of the car
 
-    speed = (leftStickY*64)+64  #The leftStickY value is between -1 and 1, converted to [0-128]
-    if speed == 128:
-        speed = 127
+    speed = (leftStickY*32)+64  #The leftStickY value is between -1 and 1, converted to [0-128] to get the whole range, and [0-64] to get only half
 
     rightStickX = 0
 
@@ -20,7 +18,7 @@ def main():
     global speed
     global steering_angle
     
-    speed = 0
+    speed = 64
     steering_angle = 0
 
     msgAckermann = AckermannDrive()
