caf0b4f42e49a6e85d2c55968aa6aded583d506b
eli-baron <ehecatl.eli.ba@gmail.com>
2019-08-29 20:57:13 -0500

    add speed value and change the roboclaw port

--- catkin_ws/src/hardware/mobile_base/scripts/mobile_base_node.py
+++ catkin_ws/src/hardware/mobile_base/scripts/mobile_base_node.py
@@ -12,13 +12,13 @@ def callbackAckermann(msg):
 def main():
 	global speed
 
-	speed = 0
+	speed = 64	#half the [0-127] to get 0 speed in ForwardBackward command
 
 	msgAckermann = AckermannDrive()
 	rospy.init_node('mobile_base_node', anonymous=True)
 	rospy.Subscriber("/ackermann", AckermannDrive, callbackAckermann)
 	rate=rospy.Rate(10)
-	rc=Roboclaw("/dev/ttyACM0",115200)
+	rc=Roboclaw("/dev/ttyACM1",115200)
 	rc.Open()
 	address=0x80
 	br = tf.TransformBroadcaster()
@@ -28,12 +28,13 @@ def main():
 		autobot_x += speed*0.001
 		msgAckermann.speed = speed
 		rc.ForwardBackwardM1(address,int(msgAckermann.speed))		#0 power forward = 64
-		rc.ForwardBackwardM1(address,int(msgAckermann.speed))	#0 power backward = 64
+		rc.ForwardBackwardM2(address,int(-msgAckermann.speed))	#0 power backward = 64
+		br.sendTransform((autobot_x, 0, 0),tf.transformations.quaternion_from_euler(0,0,0),rospy.Time.now(),"base_link","odom")
 		rate.sleep()
 	#rospy.spin()
 
 if __name__=='__main__':
 	try:
-    	main()
-    except rospy.ROSInterruptException:
-        pass
+	   	main()
+	except rospy.ROSInterruptException:
+		pass
